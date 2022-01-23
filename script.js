8e039034aac80edc1a16507276b4f29ec5e62472
Koffi Morris <koffimorris@hotmail.com>
2022-01-23 01:53:38 -0400

    created grid and sketch feature

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
-const container = document.querySelector('#container');
-let makeRows = document.getElementsByClassName("gridRow");
-let makeColumns = document.getElementsByClassName("cell");
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+let h=document.getElementsByClassName("hove")
+let rainbow=document.querySelector("#rainbow")
+const header2=document.createElement('h1');
 
-function createGrid(){
 
-    createRows(16);
-    createColumns(16);
-}
+header2.classList.add('header2');
+header2.style.color='red';
+header2.textContent=`Etch-A-Sketch`
+makeGrid(16);
 
-function createRows(rows){
+//Takes (rows, columns) input and makes a grid
+function makeGrid(Num) {
 
-    for(let i=0; i<makeRows; i++){
-        const row=document.createElement('div');
-        container.appendChild(row).className('.gridRow');
+    //Creates rows
+    for (r = 0; r < Num; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+    //creates columns
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < Num; j++) {
+             let newCell = document.createElement("div");
+             rows[j].appendChild(newCell).className = "cell";
     }
+}
+};
+function Colours() {
+    let sketchBoxes = document.querySelectorAll('.cell');
+    sketchBoxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            if(rainbow.checked===true){
+              let randomColor=  Math.floor(Math.random()*16777215).toString(16);
+              box.style.backgroundColor= "#" + randomColor;
+            }
+            else{
+            box.style.backgroundColor = 'black'
+            }
+        })    
+    })
+}
 
-}
-function createColumns(columns){
-    for(let j=0; i<makeColumns; j++){
-        const column=document.createElement('div');
-        rows[j].appendChild(column).className('.cell');
-    }
-}
+
+
+//  function hoverChange(colorNum){
+//     for (i = 0; i < rows.length; i++) {
+//         for (j = 0; j < colorNum; j++) {
+//             let newCell = document.createElement("div");
+//             rows[j].appendChild(newCell).className = "cell";
+//             rows[j].appendChild(newCell).className = "hove";
+//         };
+
+//     };
+
+// }
