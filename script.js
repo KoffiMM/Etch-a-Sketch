c27df975db7a441481723008006a012ebbebe63a
Koffi Morris <koffimorris@hotmail.com>
2022-01-24 18:52:56 -0400

    Completed Etch-A-Sketch project

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ let cells = document.getElementsByClassName("cell");
 let h=document.getElementsByClassName("hove")
 let rainbow=document.querySelector("#rainbow")
 const header2=document.createElement('h1');
+let upgrade=document.querySelector('#upgrade')
+let gridSize=16;
 
-
-header2.classList.add('header2');
-header2.style.color='red';
-header2.textContent=`Etch-A-Sketch`
-makeGrid(16);
 
 //Takes (rows, columns) input and makes a grid
-function makeGrid(Num) {
+function makeGrid(gridSize) {
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    let sketchBoxes = document.querySelectorAll('.cell');
+    if (sketchBoxes) reset(sketchBoxes);
 
+    let totalSquares=gridSize*gridSize;
+    if(totalSquares>=10&&totalSquares<=10000){
     //Creates rows
-    for (r = 0; r < Num; r++) {
+    for (r = 0; r <totalSquares; r++) {
         let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
+        container.appendChild(row).className = "cell";
     };
-    //creates columns
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < Num; j++) {
-             let newCell = document.createElement("div");
-             rows[j].appendChild(newCell).className = "cell";
-    }
+}else if(totalSquares<=9){
+    alert('Number is too low, Please try again')
 }
+
 };
+
 function Colours() {
     let sketchBoxes = document.querySelectorAll('.cell');
     sketchBoxes.forEach((box) => {
@@ -41,8 +42,19 @@ function Colours() {
         })    
     })
 }
+function reset(boxes){
+    boxes.forEach((box)=>{
+        box.remove();
+    })
+}
+function resizeGrid(){
+    let gridSize = prompt("Please provide a new Grid Size");
+    makeGrid(gridSize);
+    Colours()
+     }
 
 
+     makeGrid(gridSize);
 
 //  function hoverChange(colorNum){
 //     for (i = 0; i < rows.length; i++) {
